57137b01c1f6acf577017b565350c6a6d3d44c1f
IanivB <39414029+IanivB@users.noreply.github.com>
2018-06-07 22:45:53 +0300

    Add files via upload

--- device_manager.py
+++ device_manager.py
@@ -1,20 +1,19 @@
 
 class DeviceManager(object):
     def __init__(self):
-        self.devices_users_mapping = {}
+        self.devices_users_mapping = []
 
     def get_all_devices(self):
         return self.devices_users_mapping
 
     def add_device(self, dev):
-        self.devices_users_mapping[dev] = ''
+        # self.devices_users_mapping[dev] = ''
+        self.devices_users_mapping.append(dev)
 
     def occupy_dev(self, occ_info):
-        # dev_ip = occ_info.keys()[0]
-        # user = occ_info[dev_ip]
-        dev_ip = occ_info[0]
-        user = occ_info[1]
+        dev_ip = occ_info["ip"]
+        user = occ_info["user"]
 
-        for dev in self.devices_users_mapping.values() :
-            if (dev == dev_ip) and (self.devices_users_mapping[dev] == ""):
-                self.devices_users_mapping[dev] = user
+        for dev in self.devices_users_mapping:
+            if (dev['ip'] == dev_ip) and (dev['user'] == ""):
+                dev['user'] = user
--- server.py
+++ server.py
@@ -5,8 +5,6 @@ import json
 
 SERVER_IP = 'localhost'
 SERVER_PORT = 8000
-# devices = {'user': ['windows', 'HP', '192.168.0.20']}
-# json_devices = json.dumps(devices)
 
 
 class MyHandler(BaseHTTPRequestHandler):
@@ -18,21 +16,29 @@ class MyHandler(BaseHTTPRequestHandler):
 
         if self.path == '/api/get_devices':
             devices = self.server.dev_manager.get_all_devices()
-            json_devices = json.dumps(devices)
+            print type(devices), "devices"
+            json_devices = json.dumps(devices, encoding="utf-8")
+            print json_devices, "do get"
             self.wfile.write(json_devices)
 
     def do_POST(self):
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+
         if self.path == '/api/add_device':
-            self.send_response(200)
             content_length = int(self.headers['Content-Length'])
-            raw_data = json.dumps(self.rfile.read(content_length))
-            self.server.dev_manager.add_device(raw_data)
+            raw_data = self.rfile.read(content_length)
+            json_data = json.loads(raw_data)
+            print json_data
+            self.server.dev_manager.add_device(json_data)
 
         elif self.path == '/api/occupy_device':
-            self.responses(200)
             content_length = int(self.headers['Content-Length'])
-            print content_length
             raw_data = self.rfile.read(content_length)
+            json_data = json.loads(raw_data)
+            print json_data
             self.server.dev_manager.occupy_dev(raw_data)
 
 
